feb00a49cf4be328d620b6c8b2f8b93badda53d2
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-04-15 16:50:36 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,16 +1,20 @@
-require('../controllers/settings');
+// Mengimpor nilai-nilai global
+const {
+    mongo_Db
+} = require('../controllers/settings');
+
 const mongoose = require('mongoose');
 
 function connectToMongoDb() {
-	mongoose.connect(mongo_Db, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true
-	});
-	const db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'Connection error:'));
-	db.once('open', () => {
-		console.log('</> Success connect to MongoDb √');
-	});
-};
+    mongoose.connect(mongo_Db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', () => {
+        console.log('</> Success connect to MongoDb √');
+    });
+}
 
 module.exports.connectToMongoDb = connectToMongoDb;
