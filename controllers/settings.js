05dc806c0847e9127b9b75c87268cfa15ad26c31
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-04-15 17:32:30 +0700

    Update settings.js

--- controllers/settings.js
+++ controllers/settings.js
@@ -7,7 +7,7 @@ global.dateLimit = "Oktober, 31 2022",
 global.mongo_Db = "mongodb+srv://ajiart01:Ajifu917@cluster0.zebgfie.mongodb.net/?retryWrites=true&w=majority";
 global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
 global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
-global.my_email = "vynaavalerie@gmail.com";
-global.my_email_password = "kontol66k";
+global.my_email = "xyroineeid@gmail.com";
+global.my_email_password = "olepkldyilussbbk";
 global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
 global.port = 8080
