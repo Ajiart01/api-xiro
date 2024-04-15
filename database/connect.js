425a6beb03d5d19737a054428d4aa54d09de7c00
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-04-15 16:58:29 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,20 +1,28 @@
-// Mengimpor nilai-nilai global
+// Impor nilai-nilai global
 const {
     mongo_Db
 } = require('../controllers/settings');
 
-const mongoose = require('mongoose');
+// Memeriksa apakah nilai `mongo_Db` telah terdefinisi
+if (mongo_Db) {
+    // Jika terdefinisi, lanjutkan dengan menghubungkan ke MongoDB
+    const mongoose = require('mongoose');
 
-function connectToMongoDb() {
-    mongoose.connect(mongo_Db, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'Connection error:'));
-    db.once('open', () => {
-        console.log('</> Success connect to MongoDb √');
-    });
+    function connectToMongoDb() {
+        mongoose.connect(mongo_Db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        const db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'Connection error:'));
+        db.once('open', () => {
+            console.log('</> Success connect to MongoDb √');
+        });
+    }
+
+    module.exports.connectToMongoDb = connectToMongoDb;
+
+} else {
+    // Jika tidak terdefinisi, tampilkan pesan kesalahan
+    console.error('MongoDB connection string is not defined.');
 }
-
-module.exports.connectToMongoDb = connectToMongoDb;
