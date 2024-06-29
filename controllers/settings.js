982bec29f762f6237f8fcfa9c2ca8b958f3a3d95
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-06-29 15:43:46 +0700

    Update settings.js

--- controllers/settings.js
+++ controllers/settings.js
@@ -4,10 +4,10 @@ global.key_premium = [];
 global.limit_free = "30";
 global.limit_premium = "99999";
 global.dateLimit = "Oktober, 31 2022",
-global.mongo_Db = "mongodb+srv://ajiart01:Aji11032001@cluster0.zebgfie.mongodb.net/?retryWrites=true&w=majority";
+global.mongo_Db = "mongodb+srv://ajifu917:ajifu917@atlascluster.sws1np9.mongodb.net/myDatabase?retryWrites=true&w=majority";
 global.recaptcha_key_1 = "6LegHYwmAAAAAOmYT82OldAD5MOpo7S9g5pT0XfQ";
 global.recaptcha_key_2 = "6LegHYwmAAAAAJSGEwz5G7jCD4tbBvrT-8nx3mBV";
-global.my_email = "xyroineeid@gmail.com";
-global.my_email_password = "olepkldyilussbbk";
+global.my_email = "acaca3359@gmail.com";
+global.my_email_password = "qckjeobvkurqfqzd";
 global.activation_token = "-@Pqnap+@(/1jAPPnew/@10";
 global.port = 8080
